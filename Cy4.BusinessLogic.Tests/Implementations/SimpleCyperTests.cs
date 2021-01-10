3931efa9a96fbd887bee1b18cdce2ab5a2dcdd97
Rhys <RhysWakeham14@gmail.com>
2021-01-10 18:05:42 +0000

    Created SimpleCypher, ICypher interface and SimpleCypher tests - with working, passing tests for SImplyCypher's Encrypt implementation. SimpleCypher just shifts letters by 1 place in the alphabet (simple - right?)

---
+++ Cy4.BusinessLogic.Tests/Implementations/SimpleCyperTests.cs
@@ -0,0 +1,58 @@
+﻿using Cy4.BusinessLogic.Implementations;
+using Cy4.BusinessLogic.Interfaces;
+using NUnit.Framework;
+
+namespace Cy4.BusinessLogic.Tests.Implementations
+{
+    [TestFixture]
+    public class SimpleCyperTests
+    {
+        private ICypher _simpleCyper;
+
+        [SetUp]
+        public void Setup()
+        {
+            _simpleCyper = new SimpleCypher();
+        }
+
+        [Test]
+        public void Encrypt_ShouldShiftAllCapitalLettersByOneByDefault()
+        {
+            // Data.
+            var unencryptedValue = "ABCDEFG";
+            var expectedValue = "BCDEFGH";
+
+            // Test
+            var actualValue = _simpleCyper.Encrypt(unencryptedValue);
+
+            // Analysis.
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [Test]
+        public void Encrypt_ShouldShiftAllLowerCaseLettersByOneByDefault()
+        {
+            // Data.
+            var unencryptedValue = "abcdefg";
+            var expectedValue = "bcdefgh";
+
+            // Test
+            var actualValue = _simpleCyper.Encrypt(unencryptedValue);
+
+            // Analysis.
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestCase("AbCdEfG", "BcDeFgH")]
+        [TestCase("abcDEF", "bcdEFG")]
+        [TestCase("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", "BCDEFGHIJKLMNOPQRSTUVWXYZAbcdefghijklmnopqrstuvwxyza")]
+        public void Encrypt_ShouldShiftUpperAndLowerCaseCharactersByOneAndRetainCase(string unencryptedValue, string expectedValue)
+        {
+            // Test
+            var actualValue = _simpleCyper.Encrypt(unencryptedValue);
+
+            // Analysis
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
---
+++ Cy4.BusinessLogic/Implementations/SimpleCypher.cs
@@ -0,0 +1,89 @@
+﻿using Cy4.BusinessLogic.Interfaces;
+using System.Collections.Generic;
+
+namespace Cy4.BusinessLogic.Implementations
+{
+    public class SimpleCypher : ICypher
+    {
+        private IDictionary<char, char> _characterParings;
+        private int _defaultOffset;
+
+        public SimpleCypher()
+        {
+            _defaultOffset = 1;
+        }
+
+        public string Encrypt(string value)
+        {
+            var newValue = "";
+            ResetDict();
+            CreateEncryptionDict();
+
+            foreach(var character in value)
+            {
+                newValue += _characterParings[character];
+            }
+
+            return newValue;
+        }
+
+        public string Decrypt(string value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void ResetDict()
+        {
+            _characterParings?.Clear();
+            _characterParings = new Dictionary<char, char>();
+        }
+
+        private void CreateEncryptionDict()
+        {
+            CreateUpperCasePairings();
+            CreateLowerCasePairings();
+        }
+
+        private void CreateLowerCasePairings()
+        {
+            var lowerCaseChars = new List<char>
+            {
+                'a','b','c','d','e','f','g','h','i','j','k','l','m',
+                'n','o','p','q','r','s','t','u','v','w','x','y','z'
+            };
+
+            for (int i = 0; i < 26; i++)
+            {
+                var pairIndex = GetPairIndex(i, 1, 25);
+                _characterParings.Add(lowerCaseChars[i], lowerCaseChars[pairIndex]);
+            }
+        }
+
+        private void CreateUpperCasePairings()
+        {
+            var upperCaseChars = new List<char>
+            {
+                'A','B','C','D','E','F','G','H','I','J','K','L','M',
+                'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+            };
+
+            for (int i = 0; i < 26; i++)
+            {
+                var pairIndex = GetPairIndex(i, 1, 25);
+                _characterParings.Add(upperCaseChars[i], upperCaseChars[pairIndex]);
+            }
+        }
+
+        private int GetPairIndex(int currentIndex, int offset, int maxIndex)
+        {
+            var pairIndex = currentIndex + offset;
+
+            while (pairIndex > maxIndex)
+            {
+                pairIndex -= maxIndex + 1;
+            }
+
+            return pairIndex;
+        }
+    }
+}
